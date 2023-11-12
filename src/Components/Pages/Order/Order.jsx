fdcccb52d47e5c9e7465add0ac29eee4cc6da36e
chand-stack <chand.stack@gmail.com>
2023-11-13 02:58:38 +0600

    Chore: shop page added with dynamic cards

--- src/Components/Pages/Order/Order.jsx
+++ src/Components/Pages/Order/Order.jsx
@@ -4,10 +4,11 @@ import Cover from '../../Shared/Cover/Cover';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import 'react-tabs/style/react-tabs.css';
 import useMenu from '../../../Hook/useMenu';
+import ShopCard from '../../Shared/ShopCard/ShopCard';
 const Order = () => {
     const [tabIndex,setTabIndex] = useState(0)
     const [menu] = useMenu()
-    const offer = menu.filter(item => item.category === "offered")
+    const drink = menu.filter(item => item.category === "drinks")
     const dessert = menu.filter(item => item.category === "dessert")
     const pizza = menu.filter(item => item.category ===  "pizza")
     const salad = menu.filter(item => item.category ===  "salad")
@@ -25,8 +26,41 @@ const Order = () => {
         <Tab>DESSERTS</Tab>
         <Tab>DRINKS</Tab>
       </TabList>
-      <TabPanel></TabPanel>
-      <TabPanel></TabPanel>
+      <TabPanel>
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
+          {
+            salad?.map(item => <ShopCard key={item._id} item={item}></ShopCard>)
+          }
+        </div>
+      </TabPanel>
+      <TabPanel>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
+          {
+            pizza?.map(item => <ShopCard key={item._id} item={item}></ShopCard>)
+          }
+        </div>
+      </TabPanel>
+      <TabPanel>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
+          {
+            soup?.map(item => <ShopCard key={item._id} item={item}></ShopCard>)
+          }
+        </div>
+      </TabPanel>
+      <TabPanel>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
+          {
+            dessert?.map(item => <ShopCard key={item._id} item={item}></ShopCard>)
+          }
+        </div>
+      </TabPanel>
+      <TabPanel>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12'>
+          {
+            drink?.map(item => <ShopCard key={item._id} item={item}></ShopCard>)
+          }
+        </div>
+      </TabPanel>
     </Tabs>
 
             </div>
--- src/Components/Shared/ShopCard/ShopCard.jsx
+++ src/Components/Shared/ShopCard/ShopCard.jsx
@@ -1,15 +1,16 @@
 
 const ShopCard = ({item}) => {
-    const {price,recipe,name,image} = item
+    const {price,recipe,name,image} = item;
     return (
         <div>
-            <div className="card w-96 bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg" alt="Shoes" /></figure>
-  <div className="card-body">
-    <h2 className="card-title">Shoes!</h2>
-    <p>If a dog chews shoes whose shoes does he choose?</p>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+            <div className="card  rounded-none">
+  <figure><img className="h-[300px] w-full" src={image} alt="Shoes" /></figure>
+  <p className="text-white bg-black font-semibold absolute right-0 mt-5 mr-5 py-1 px-3">${price}</p>
+  <div className="card-body text-center bg-gray-100">
+    <h2 className="card-title flex-grow justify-center">{name}</h2>
+    <p className="flex-grow">{recipe}</p>
+    <div className="card-actions justify-center">
+      <button className="btn btn-outline border-t-0 border-l-0 border-r-0 border-b-2 text-[#BB8506] hover:text-[#BB8506] border-[#BB8506]">ADD TO CART</button>
     </div>
   </div>
 </div>
